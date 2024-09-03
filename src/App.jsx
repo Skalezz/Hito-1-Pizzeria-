d8ec8ce09692e0e5c4b129491fa643c4c6ddf2d7
Esteban Velimir Skarneo Gonzalez <154566477+Skalezz@users.noreply.github.com>
2024-09-02 21:48:00 -0400

    hito5

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-
-
-
-
-
-
-
-
-// import Home from './pages/Home'
-// import Header from './components/Header'
-// import Navbar from './components/Navbar'
-// import Footer from './components/Footer'
-// import Register from './pages/Register'
-// import Login from './pages/Login'
-// import Cart from './pages/Cart'
-// import productos from './pizzas.json'
-// import Pizza from './components/Pizza'
-
-
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Register from './pages/Register';
+import Header from './components/Header';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+import Pizza from './pages/Pizza';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import productos from './pizzas.json'
 
 function App  () {
 
   return (
     <>
-    
+      <Navbar/>  
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart productos={productos}/>} />
+        <Route path='/pizza' element={<Pizza/>} />
+        <Route path='/register' element={<Register/>  } />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+      <Footer/>  
     </>
   )
 }
